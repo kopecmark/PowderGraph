eebc39da8516e17ba83a2024047bf75e17d76032
Mark Kopec <mark.kopec@gmail.com>
2019-01-11 10:16:37 -0800

    Add margins for label axis

--- js/main.js
+++ js/main.js
@@ -5,15 +5,37 @@ d3.csv("data/ll_monthly_snow.csv").then(function(data){
     month.Year = +month.Year;
   })
   console.log(data);
+    var margin = { left:100, right: 10, top:10, bottom: 100}
+
+    var width = 1500 - margin.left - margin.right;
+    var height = 500 - margin.top - margin.bottom;
 
     var svg = d3.select("#chart-area").append("svg")
-      .attr("width", 5000 )
-      .attr("height", 500)
+      .attr("width", width + margin.left + margin.right )
+      .attr("height", height + margin.top + margin.bottom)
 
-
+      var min = d3.min(data, (month) => {
+        return month.totalSnow
+      })
+      
+      var max = d3.max(data, (month) => {
+        return month.totalSnow
+      })
+      
     var y = d3.scaleLinear()
-      .domain([0,400])
-      .range([0,500])
+      .domain([min, max])
+      .range([0, height])
+
+    console.log('min y axis val ' + min)
+    console.log('max y axis val ' + max)
+
+    var x = d3.scaleBand()
+      .domain(data.map((month) => { 
+        return month['Date/Time']
+      }))
+      .range([0,width])
+      .paddingInner(1)
+      .paddingOuter(1)
 
     // var x = d3.scaleTime()
     //   .domain([new Date(1915,1,1), 
@@ -22,16 +44,19 @@ d3.csv("data/ll_monthly_snow.csv").then(function(data){
 
     var rect = svg.selectAll("rect")
       .data(data);
-
+  
+ 
     rect.enter()
       .append("rect")
-      .attr("x", (b, i) => {
-        return (i * 10) + 10;
+      .attr("x", (m) => {
+        return x(m['Date/Time'])
       })
       .attr("y", 10)
-      .attr("width", 5)
-      .attr("height", (m, i) => {
+      .attr("width", 2)
+      .attr("height", (m) => {
         return y(m.totalSnow);
       })
+      .attr("fill", "green");
   
 })
+
