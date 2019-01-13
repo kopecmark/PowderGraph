41529ae29b79f07cefa985e16965f7a5a587a3c9
Mark Kopec <mark.kopec@gmail.com>
2019-01-12 17:55:22 -0800

    Setup interval updates

--- js/main.js
+++ js/main.js
@@ -11,9 +11,29 @@ var g = d3.select("#chart-area")
   .append("g")
     .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
 
+var xAxisGroup = g.append("g")
+  .attr("class", "x-axis")
+  .attr("transform", "translate(0, " + height + ")")
+  
+
+var yAxisGroup = g.append("g")
+  .attr("class", "y-axis")
+ 
+
+
+// Scale the axises
+var x = d3.scaleBand()
+  .range([0, width])
+  .paddingInner(1)
+  .paddingOuter(1)
+
+var y = d3.scaleLinear()
+  .range([height, 0])
+
+
 // X Label Name
 g.append("text")
-  .attr("class", "x axis-label")
+  // .attr("class", "x axis-label")
   .attr("x", width / 2)
   .attr("y", height + 80)
   .attr("font-size", "15px")
@@ -22,7 +42,7 @@ g.append("text")
 
 // Y Label Name
 g.append("text")
-  .attr("class", "y axis-label")
+  // .attr("class", "y axis-label")
   .attr("x", - (height / 2))
   .attr("y", -60)
   .attr("font-size", "15px")
@@ -32,7 +52,7 @@ g.append("text")
 
   
 d3.csv("data/ll_monthly_snow.csv").then(function(data){
-  data.forEach(month=>{
+  data.forEach( (month) =>{
     month.totalSnow = Number(month['Total Snow (cm)']);
     month.Month = +month.Month;
     month.Year = +month.Year;
@@ -40,40 +60,34 @@ d3.csv("data/ll_monthly_snow.csv").then(function(data){
 
   console.log(data);
 
-  var min = d3.min(data, (month) => {
-    return month.totalSnow
-  })
+  d3.interval(function(){
+    update(data)
+  }, 1000);
+
+  // Run the vis for the first time
+  update(data);
+})
+
+function update(data) {
+  // Update the domain of each axis
+  // var min = d3.min(data, (month) => {
+  //   return month.totalSnow
+  // })
 
   var max = d3.max(data, (month) => {
     return month.totalSnow
   })
 
-  var x = d3.scaleBand()
-    .domain(data.map((month) => {
-      return month['Date/Time']
-    }))
-    .range([0, width])
-    .paddingInner(1)
-    .paddingOuter(1)
+  x.domain(data.map((month) => {
+    return month['Date/Time']
+  }))
 
-  var y = d3.scaleLinear()
-    .domain([min, max])
-    .range([height, 0])
+  y.domain([0, max])
 
-  
   // X axis
   var xAxisCall = d3.axisBottom(x);
 
-  g.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", "translate(0, " + height + ")")
-    .call(xAxisCall)
-    .selectAll("text")
-    .attr("y", "10")
-    .attr("x", "-5")
-    .attr("text-anchor", "end")
-    .attr("transform", "rotate(-40)");
-
+  
   // Y axis
   var yAxisCall = d3.axisLeft(y)
     .ticks(10)
@@ -81,33 +95,38 @@ d3.csv("data/ll_monthly_snow.csv").then(function(data){
       return snowLevel + "cm"
     });
 
-  g.append("g")
-    .attr("class", "y-axis")
-    .call(yAxisCall);
+  xAxisGroup.call(xAxisCall)
+    .selectAll("text")
+    .attr("y", "10")
+    .attr("x", "-5")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-40)");
 
+  yAxisGroup.call(yAxisCall);
+
+  // JOIN new data with old elements
   var rect = g.selectAll("rect")
     .data(data);
 
-    console.log('min y axis val ' + min)
-    console.log('max y axis val ' + max)
- 
+  // EXIT old elements not present in new data
+  rect.exit().remove();
+
+  // UPDATE old elements present in new data
+  rect
+    .attr("x", (m) => {return x(m['Date/Time'])})
+    .attr("y", (m) => { return y(m.totalSnow) })
+    .attr("width", 2)
+    .attr("height", (m) => {return height - y(m.totalSnow); })
+
+  // console.log('min y axis val ' + min)
+  // console.log('max y axis val ' + max)
+
   rect.enter()
     .append("rect")
-    .attr("x", (m) => {
-      return x(m['Date/Time'])
-    })
-    .attr("y", m => { return y(m.totalSnow)})
-    .attr("width", 2)
-    .attr("height", (m) => {
-      return height - y(m.totalSnow);
-    })
-    .attr("fill", "green");
-  
-  d3.interval(function(){
-    update(data)
-  }, 1000)
-})
-
-function update(data) {
+      .attr("x", (m) => {return x(m['Date/Time'])})
+      .attr("y", (m) => { return y(m.totalSnow) })
+      .attr("width", 2)
+      .attr("height", (m) => {return height - y(m.totalSnow); })
+      .attr("fill", "green");
 
 }
