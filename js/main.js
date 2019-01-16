e03be9d5daab6b26d0de1edc28589672d0d69f55
Mark Kopec <mark.kopec@gmail.com>
2019-01-15 22:56:46 -0800

    Update slider value when selected

--- js/main.js
+++ js/main.js
@@ -118,6 +118,10 @@ var output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+  output.innerHTML = this.value;
+  year = Number(this.value);
+}
 slider.onchange = function () {
   output.innerHTML = this.value;
   year = Number(this.value);
