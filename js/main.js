ca946a4121fcdf905da58cd3bdd2d076e5ea253a
Mark Kopec <mark.kopec@gmail.com>
2019-03-18 12:50:14 -0600

    Add data parsing for line chart to mainjs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 var flag = true;
 var formattedData;
+var formattedData2;
 var year = 1919;
 var barChart;
+var lineChart;
+var formattedDataYearly = [];
+var allYears = [];
+var maxSnow = 0;
+var maxRain = 0;
 
 
 d3.csv("data/ll_monthly_snow.csv").then(function(data){
@@ -25,13 +31,54 @@ d3.csv("data/ll_monthly_snow.csv").then(function(data){
   });
 
   barChart = new BarChart("#chart-area-bar", selectedData);
-  lineChart = new LineChart("#chart-area-line", selectedData);
+});
 
-})
+
+d3.csv("data/ll_monthly_snow.csv").then(function (data) {
+  formattedData2 = data.map((month) => {
+
+    const newMonth = {};
+    newMonth.totalSnow = Number(month['Total Snow (cm)']);
+    newMonth.Month = +month.Month;
+    newMonth.Year = +month.Year;
+    newMonth.totalRain = Math.round(Number(month['Total Rain (mm)']) / 10);
+    var parseTime = d3.timeParse("%Y-%m");
+    var formatTime = d3.timeFormat("%b");
+    newMonth['MonthText'] = formatTime(parseTime(month['Date/Time']));
+
+    if (!allYears.includes(newMonth.Year)) {
+      allYears.push(newMonth.Year);
+    }
+
+    return newMonth;
+  });
+
+
+  // format data for use in a line graph
+  allYears.forEach(year => {
+    var singleYear = { year: year, values: [] };
+
+    var allMonths = formattedData.filter(month => {
+      return month.Year === year;
+    });
+
+    allMonths.forEach(month => {
+      singleYear.values.push({ month: month.Month, totalSnow: month.totalSnow, totalRain: month.totalRain });
+      if (month.totalSnow > maxSnow) maxSnow = month.totalSnow;
+      if (month.totalRain > maxRain) maxRain = month.totalRain;
+
+    });
+
+    formattedDataYearly.push(singleYear);
+  });
+
+  lineChart = new LineChart("#chart-area-line", formattedDataYearly);
+
+});
+
 
 // Function to switch between snow and rain
-
-let button = document.getElementById("precip-button")
+let button = document.getElementById("precip-button");
   
 button.onclick = () => {
   if (button.innerHTML == "Snow") {
@@ -47,7 +94,9 @@ button.onclick = () => {
   });
 
   barChart.wrangleData(selectedData);
-}
+  lineChart.wrangleData(formattedDataYearly);
+};
+
 
 // Slider
 var slider = document.getElementById("myRange");
